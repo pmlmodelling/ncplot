b6075dabd50232280e7d4dcb5767f262d6f76887
Robert Wilson <robertwilson190@gmail.com>
2020-10-12 17:59:16 +0100

    add file existence check

--- ncplot/command_line.py
+++ ncplot/command_line.py
@@ -1,10 +1,17 @@
 
 from ncplot import ncplot
 import sys
+import os
 
 
 def main():
+    if len(sys.argv) == 1:
+        return "Please supply a file"
     ff = sys.argv[1]
+
+    if os.path.exists(ff) == False:
+        return f"{ff} does not exist!"
+
     return ncplot(ff)
     print(ascii_snek)
 
