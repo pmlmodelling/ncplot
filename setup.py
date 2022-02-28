8ab9f5f3747f6ad130eb9d6c5cd99f2644da77b2
Robert  Wilson <robertwilson190@gmail.com>
2022-02-28 15:32:14 +0000

    update to 0.1.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except IOError:
 
 
 setup(name='ncplot',
-      version='0.1.1',
+      version='0.1.2',
       description=DESCRIPTION,
       long_description=long_description,
       long_description_content_type='text/markdown',
