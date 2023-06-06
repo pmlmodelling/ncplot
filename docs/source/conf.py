642681d26178876c299c15f022c364e916d3bbfb
Robert  Wilson <robertwilson190@gmail.com>
2023-06-06 17:39:44 +0100

    try material theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ extensions = [
 
 #html_theme = "sphinx_rtd_theme"
 #html_theme = "haiku"
-html_theme = "sphinx_theme_pd"
+html_theme = "sphinx_material"
 
 
 # Add any paths that contain templates here, relative to this directory.
